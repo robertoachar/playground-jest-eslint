b0b0863e2463793830a52d5f725a84d28b11e32a
Roberto Achar <robertoachar@gmail.com>
2019-11-08 17:14:34 -0300

    style: add lint

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@ module.exports = {
   projects: [
     {
       displayName: 'test'
+    },
+    {
+      runner: 'jest-runner-eslint',
+      displayName: 'lint',
+      testMatch: ['<rootDir>/src/**/*.js']
     }
-  ]
+  ],
+  watchPlugins: ['jest-runner-eslint/watch-fix']
 };
